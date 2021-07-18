d59dec8012c02b4f1d661ad2fa99c5304606eab8
Bennet Yousuf <bennetyousuf@Bennets-MacBook-Pro.local>
2021-07-17 22:24:48 -0500

    update app.js file: create references, for loop to append values to table, event listener, print values to page

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,58 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Preview raw data from data.js in console
+// console.log(tableData);
+
+
+// Creating References for D3 calls
+const $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+
+
+
+// Input data into table, loop through each row in dataInput to append values to a table cell
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
+
+
+// Creating an Event Listener for the Button
+// Setting up the Filter Button for Date and City
+button.on("click", () => {
+
+    d3.event.preventDefault();
+    
+
+    var inputDate = inputFieldDate.property("value").trim();
+    // console.log(inputDate)    
+
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    
+
+    $tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+
+    if(response.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+    
+        else {
+            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+        }
+})
